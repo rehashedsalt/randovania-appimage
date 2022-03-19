04b7b363abd55f455d18fc5a03b0deef3bb8c11f
Jacob Babor <jacob@babor.tech>
2022-03-19 15:13:37 -0500

    We're up to a functional implementation of the build process now. Ish. We're still missing a few critical components, such as: * Mono, in its entirety * The whole zipping-up part of making an AppImage But it's there, and it works

--- build.sh
+++ build.sh
@@ -36,18 +36,28 @@ wget "$python_appimage"
 chmod +x python*-manylinux1_x86_64.AppImage
 ./python*-manylinux1_x86_64.AppImage --appimage-extract
 
+# Add squashfs-root/usr/bin to the front of PATH so the right python
+# interp gets used for the build process
+PATH="$PWD"/squashfs-root/usr/bin:"$PATH"
+which python
+
 # Grab Randovania, check out the ref we want
 git clone "$randovania_git_uri" randovania
 git -C randovania fetch --tags
 git -C randovania checkout "$randovania_git_ref"
 
-# Make a directory for Randovania and sync it over
+# Make a directory for Randovania in the image and sync it over
 mkdir -p squashfs-root/"$randovania_location"
 rsync -aHS --exclude={.git} randovania/ squashfs-root/"$randovania_location"
 
 # Install Randovania
 pushd squashfs-root/"$randovania_location"
 tools/prepare_virtual_env.sh
+# Drop to a subshell to build the UI real quick
+(
+	. venv/bin/activate
+	python setup.py build_ui
+)
 # I'm not building a whole release just to get this one json file lol
 cat > randovania/data/configuration.json << EOF
 {"discord_client_id": 618134325921316864, "server_address": "https://randovania.metroidprime.run/randovania", "socketio_path": "/randovania/socket.io"}
@@ -55,4 +65,4 @@ EOF
 popd
 
 # We're all wrapped up; time to modify AppRun to start Randovania
-sed -ie 's|"$@"|"-m" "randovania"|g'
+cp ../apprun-randovania.sh "$AppRun" -f
